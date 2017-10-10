55382203dc5ef677f54bce79921586594b6d246f
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-10-09 23:02:40 -0300

    WIP: run-ep.sh

---
+++ experiment/ep/run-ep.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+INPUT_FILE='./ep-configuration.csv'
+
+echo "Creating singularity containers..."
+#echo "$(./ep-singularity-setup)"
+echo "Singularity containers created successfully."
+
+
+echo "Running experiments from input CSV file..."
+OLDIFS=$IFS
+IFS=","
+while read name order number rp environment context parallelism block exectime
+do
+    if [ $environment = "singularity" ]; then
+        if [ $context = "mpi" ]; then
+            EXEC_COMMAND="time mpirun ./images/mpi-c-$parallelism.img"
+        elif [ $context = "mpi-high-comm" ]; then
+            # TODO
+            EXEC_COMMAND="time high_comm mpirun ./images/mpi-c-$parallelism.img"
+        else #OpenMP
+            EXEC_COMMAND="time OMP_NUM_THREADS=$parallelism ./images/omp-c.img"
+        fi
+    elif [ $environment = "docker" ]; then
+        if [ $context = "mpi" ]; then
+            EXEC_COMMAND="docker mpi ep c 16"
+        elif [ $context = "mpi-high-comm" ]; then
+            EXEC_COMMAND="docker mpi-high-comm ep c 16"
+        else # OpenMP
+            EXEC_COMMAND="docker openmp ep c 16"
+        fi
+    else # native
+        if [ $context = "mpi" ]; then
+            EXEC_COMMAND="time mpirun --hostfile file ep.C.$parallelism"
+        elif [ $context = "mpi-high-comm" ]; then
+            EXEC_COMMAND="time mpirun --hostfile file2 ep.C.$parallelism"
+        else # OpenMP
+            EXEC_COMMAND="time OMP_NUM_THREADS=$parallelism ./ep.C.x"
+        fi
+    fi
+
+    # Exclude first line
+    if [ $name != "name" ]; then
+        echo "$EXEC_COMMAND";
+    fi
+done < $INPUT_FILE
+IFS=$OLDIFS
+
+echo "Done."
