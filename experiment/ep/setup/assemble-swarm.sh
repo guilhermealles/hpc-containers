adf6bd662013cbf84e8f53c0f8e9145b2b6f3dbd
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-10-16 00:08:05 -0200

    Fixes assemble-swarm script

--- experiment/ep/setup/assemble-swarm.sh
+++ experiment/ep/setup/assemble-swarm.sh
@@ -2,27 +2,23 @@
 
 if [ -z $1 ] || [ -z $2 ]; then
     echo "Usage: $0 <create/destroy> <hostfile>"
-    return
+    exit
 fi
 
 COMMAND=$1
 HOSTFILE=$2
 
 if [ $COMMAND = "create" ]; then
-    SWARM_COMMAND=$(docker swarm init | grep 'docker swarm join' | sed 's/To\sadd\sa\smanager.*$//')
+    SWARM_COMMAND=$(docker swarm init | tr '\\\n' ' ' | sed 's/Swarm initialized.*command:\(.*\)To add a manager.*/\1/')
+    echo $SWARM_COMMAND
 elif [ $COMMAND = "destroy" ]; then
-    SWARM_COMMAND="docker swarm leave"
+    SWARM_COMMAND="docker swarm leave --force"
 fi
 
-OLDIFS=$IFS
-IFS='\n'
-while read LINE
+while read -u 10 LINE
 do
     HOST=$(echo $LINE | sed 's/ .*$//')
-    ssh alles@$HOST $SWARM_COMMAND
-done < $HOSTFILE
-IFS=$OLDIFS
-
-if [ $COMMAND = "destroy" ]; then
-    docker swarm leave --force
-fi
+    echo "Processing $COMMAND on host $HOST"
+    echo $(ssh alles@$HOST "$SWARM_COMMAND")
+    echo "Host $HOST processed."
+done 10< $HOSTFILE
