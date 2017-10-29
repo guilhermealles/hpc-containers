10a9762c653ede34fbf32d161fecc9f6c570fed5
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-10-29 16:02:25 -0200

    Reduces size of singularity images to 250M

--- ep/setup/ep-singularity-setup.sh
+++ ep/setup/ep-singularity-setup.sh
@@ -7,14 +7,14 @@ fi
 
 SINGULARITY_IMAGES_DIRECTORY=$1
 
-singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-16.img"
+singularity create -s 250 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-16.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-16.img" ../singularity/alpine-mpi-ep-16.bootstrap
 
-singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-4.img"
+singularity create -s 250 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-4.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-4.img" ../singularity/alpine-mpi-ep-4.bootstrap
 
-singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-1.img"
+singularity create -s 250 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-1.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ep-1.img" ../singularity/alpine-mpi-ep-1.bootstrap
 
-singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ep.img"
+singularity create -s 250 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ep.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ep.img" ../singularity/alpine-omp-ep.bootstrap
