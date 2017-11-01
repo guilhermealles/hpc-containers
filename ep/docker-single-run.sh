25f25aba3cd740b68a76eeaf6cfc82abe5112b1d
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-10-31 22:18:55 -0200

    Docker run now builds swarm with the correct hostfile

--- ep/docker-single-run.sh
+++ ep/docker-single-run.sh
@@ -9,7 +9,9 @@ DOCKER_CLUSTER_DIR="$PWD/docker-cluster/"
 EXPERIMENT_HOME_DIR="$PWD"
 SOFTWARE_UTILS_DIR="$EXPERIMENT_HOME_DIR/../software/utils"
 HOSTFILE="$EXPERIMENT_HOME_DIR/config/hosts.txt"
-HOSTFILE_FORCE_COMM="$EXPERIMENT_HOME_DIR/config/hosts-force-comm.txt";
+HOSTFILE_FORCE_COMM="$EXPERIMENT_HOME_DIR/config/hosts-force-comm.txt"
+SWARM_HOSTFILE="$EXPERIMENT_HOME_DIR/setup/swarm_hosts.txt"
+SWARM_HOSTFILE_FORCE_COMM="$EXPERIMENT_HOME_DIR/setup/swarm_hosts-force-comm.txt"
 RESULTS_FILE="$EXPERIMENT_HOME_DIR/results/ep-results.csv"
 NAME=$1
 ENVIRONMENT=$2
@@ -17,17 +19,17 @@ CONTEXT=$3
 PARALLELISM=$4
 
 if [ $CONTEXT = "mpi" ]; then
-    ./setup/assemble-swarm.sh create $HOSTFILE
+    ./setup/assemble-swarm.sh create $SWARM_HOSTFILE
     ./setup/ep-start-docker-cluster.sh up $PARALLELISM
     cd $DOCKER_CLUSTER_DIR
     EXEC_TIME=$($SOFTWARE_UTILS_DIR/ms-time.sh ./swarm.sh exec mpirun -np $PARALLELISM NPB3.3.1/NPB3.3-MPI/bin/ep.B.$PARALLELISM)
 elif [ $CONTEXT = "mpi-high-comm" ]; then
-    ./setup/assemble-swarm.sh create $HOSTFILE_FORCE_COMM
+    ./setup/assemble-swarm.sh create $SWARM_HOSTFILE_FORCE_COMM
     ./setup/ep-start-docker-cluster.sh up $PARALLELISM
     cd $DOCKER_CLUSTER_DIR
 	EXEC_TIME=$($SOFTWARE_UTILS_DIR/ms-time.sh ./swarm.sh exec mpirun -np $PARALLELISM NPB3.3.1/NPB3.3-MPI/bin/ep.B.$PARALLELISM)
 else # OpenMP
-    ./setup/assemble-swarm.sh create $HOSTFILE
+    ./setup/assemble-swarm.sh create $SWARM_HOSTFILE
     ./setup/ep-start-docker-cluster.sh up 1
     cd $DOCKER_CLUSTER_DIR
     EXEC_TIME=$($SOFTWARE_UTILS_DIR/ms-time.sh ./swarm.sh exec OMP_NUM_THREADS=$PARALLELISM NPB3.3.1/NPB3.3-OMP/bin/ep.B.x)
@@ -35,7 +37,7 @@ fi
 
 cd "$EXPERIMENT_HOME_DIR"
 ./setup/ep-start-docker-cluster.sh down 16
-./setup/assemble-swarm.sh destroy $HOSTFILE
+./setup/assemble-swarm.sh destroy $SWARM_HOSTFILE
 
 echo ">>>>>>>>>> $NAME,$ENVIRONMENT,$CONTEXT,$PARALLELISM,$EXEC_TIME <<<<<<<<<<"
 echo "$NAME,$ENVIRONMENT,$CONTEXT,$PARALLELISM,$EXEC_TIME" >> $RESULTS_FILE
