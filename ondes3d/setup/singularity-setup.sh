66b205f187b4c9f04e01d8d0df2a8cefe8ca7428
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-06 19:45:11 -0200

    Increasis singularity image file size

--- ondes3d/setup/singularity-setup.sh
+++ ondes3d/setup/singularity-setup.sh
@@ -7,8 +7,8 @@ fi
 
 SINGULARITY_IMAGES_DIRECTORY=$1
 
-singularity create -s 250 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img"
+singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img" ../singularity/alpine-mpi-ondes3d.bootstrap
 
-singularity create -s 250 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img"
+singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img" ../singularity/alpine-omp-ondes3d.bootstrap
