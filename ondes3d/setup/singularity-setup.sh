bbbd839695642af78357984beb3152eceac136dc
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-06 20:36:25 -0200

    Fixes

--- ondes3d/setup/singularity-setup.sh
+++ ondes3d/setup/singularity-setup.sh
@@ -7,8 +7,10 @@ fi
 
 SINGULARITY_IMAGES_DIRECTORY=$1
 
+[ ! -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi ] && mkdir "$SINGULARITY_IMAGES_DIRECTORY/ondes3d_mpi"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img" ../singularity/alpine-mpi-ondes3d.bootstrap
 
+[ ! -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp ] && mkdir "$SINGULARITY_IMAGES_DIRECTORY/ondes3d_omp"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img" ../singularity/alpine-omp-ondes3d.bootstrap
