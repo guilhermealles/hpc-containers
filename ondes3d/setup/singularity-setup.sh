c94f813f9035e9e1493f65038547ea7d805fee04
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-06 22:08:17 -0200

    typo fix

--- ondes3d/setup/singularity-setup.sh
+++ ondes3d/setup/singularity-setup.sh
@@ -8,11 +8,11 @@ fi
 SINGULARITY_IMAGES_DIRECTORY=$1
 
 [ ! -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi ] && mkdir "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi"
-cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d_mpi"
+cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img" ../singularity/alpine-mpi-ondes3d.bootstrap
 
 [ ! -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp ] && mkdir "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
-cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d_omp"
+cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img" ../singularity/alpine-omp-ondes3d.bootstrap
