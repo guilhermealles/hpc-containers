85904a493aab951efe56431250f9f0d14c06fc73
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-11 18:55:19 -0200

    Fixes singularity definition file

--- ondes3d/setup/singularity-setup.sh
+++ ondes3d/setup/singularity-setup.sh
@@ -5,10 +5,12 @@ if [ -z $1 ]; then
     exit
 fi
 
+cd "$(dirname "$0")"
+
 SINGULARITY_IMAGES_DIRECTORY=$1
 
 [ ! -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi ] && mkdir "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi"
-cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi"
+cp -r ../docker-cluster/project "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img" ../singularity/alpine-mpi-ondes3d.bootstrap
 cd $SINGULARITY_IMAGES_DIRECTORY
@@ -16,7 +18,7 @@ chmod +x ./ondes3d-mpi/compile-mpi.sh
 singularity exec alpine-mpi-ondes3d.img ./ondes3d-mpi/compile-mpi.sh
 
 [ ! -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp ] && mkdir "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
-cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
+cp -r ../docker-cluster/project "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img" ../singularity/alpine-omp-ondes3d.bootstrap
 cd $SINGULARITY_IMAGES_DIRECTORY
