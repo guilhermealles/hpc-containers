900007ad326150f28f8a5c2c26c69719d2c03009
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-11 19:04:04 -0200

    .

--- ondes3d/setup/singularity-setup.sh
+++ ondes3d/setup/singularity-setup.sh
@@ -5,8 +5,9 @@ if [ -z $1 ]; then
     exit
 fi
 SINGULARITY_IMAGES_DIRECTORY=$1
+SCRIPT_DIRECTORY="$(dirname "$0")"
 
-cd "$(dirname "$0")"
+cd $SCRIPT_DIRECTORY
 [ -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi ] && rm -rf $SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi
 cp -r ../docker-cluster/project "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img"
@@ -15,7 +16,7 @@ cd $SINGULARITY_IMAGES_DIRECTORY
 chmod +x ./ondes3d-mpi/compile-mpi.sh
 singularity exec alpine-mpi-ondes3d.img ./ondes3d-mpi/compile-mpi.sh
 
-cd "$(dirname "$0")"
+cd $SCRIPT_DIRECTORY
 [ -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp ] && rm -rf $SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp
 cp -r ../docker-cluster/project "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img"
