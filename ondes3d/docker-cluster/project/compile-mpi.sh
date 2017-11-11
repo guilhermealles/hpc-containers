0bbc6d0174e3d7a45f4d88b291e1af074f8f1975
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-11 17:50:28 -0200

    Changes Ondes3D compile scripts

--- ondes3d/docker-cluster/project/compile-mpi.sh
+++ ondes3d/docker-cluster/project/compile-mpi.sh
@@ -5,4 +5,4 @@ sed -i 's/^#\(TESTFLAGS.*+=.*-DMPI\)$/\1/' ./SRC/Makefile
 sed -i 's/^\(TESTFLAGS.*+=.*-DOMP\)$/#\1/' ./SRC/Makefile
 sed -i 's/^\(.*-fopenmp\)$/#\1/' ./SRC/Makefile
 cd SRC/ && make clean && make
-cd .. && mv ondes3d ondes3d-mpi
+cd ..
