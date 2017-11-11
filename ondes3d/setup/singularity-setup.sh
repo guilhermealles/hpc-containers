bf9a7b2d8c43e886e9dda97344fc799300312210
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-11 18:46:08 -0200

    Fixes singularity setup

--- ondes3d/setup/singularity-setup.sh
+++ ondes3d/setup/singularity-setup.sh
@@ -11,8 +11,14 @@ SINGULARITY_IMAGES_DIRECTORY=$1
 cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-mpi-ondes3d.img" ../singularity/alpine-mpi-ondes3d.bootstrap
+cd $SINGULARITY_IMAGES_DIRECTORY
+chmod +x ./ondes3d-mpi/compile-mpi.sh
+singularity exec alpine-mpi-ondes3d.img ./ondes3d-mpi/compile-mpi.sh
 
 [ ! -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp ] && mkdir "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
 cp -R ../docker-cluster/project/* "$SINGULARITY_IMAGES_DIRECTORY/ondes3d-omp"
 singularity create -s 300 "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img"
 sudo singularity bootstrap "$SINGULARITY_IMAGES_DIRECTORY/alpine-omp-ondes3d.img" ../singularity/alpine-omp-ondes3d.bootstrap
+cd $SINGULARITY_IMAGES_DIRECTORY
+chmod +x ./ondes3d-omp/compile-omp.sh
+singularity exec alpine-omp-ondes3d.img ./ondes3d-omp/compile-omp.sh
