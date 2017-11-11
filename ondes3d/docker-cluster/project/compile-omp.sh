6349a5252866527e40484ee7cc54fbfd8b214b01
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-11-11 17:41:07 -0200

    Adds script to compile Ondes3d. Changes bootstrap images.

---
+++ ondes3d/docker-cluster/project/compile-omp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm ./SRC/options.h && cp ./ESSAI-XML/options.h ./SRC
+sed -i 's/^\(TESTFLAGS.*+=.*-DMPI\)$/#\1/' ./SRC/Makefile
+sed -i 's/^#\(TESTFLAGS.*+=.*-DOMP\)$/\1/' ./SRC/Makefile
+sed -i 's/^#\(.*-fopenmp\)$/\1/' ./SRC/Makefile
+cd SRC/ && make clean && make
+cd .. && mv ondes3d ondes3d-omp
