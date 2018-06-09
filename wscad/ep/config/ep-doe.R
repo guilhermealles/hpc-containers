7b87d771b2279787d4a71f85f737590eb1b4bdf6
Guilherme Alles <guilherme.r.alles@gmail.com>
2018-06-09 11:40:53 -0300

    Adds configuration for experiments

--- wscad/ep/config/ep-doe.R
+++ wscad/ep/config/ep-doe.R
@@ -1,16 +1,15 @@
 require(DoE.base);
 
 ep_DoE <- fac.design (
-    nfactors = 3,
+    nfactors = 2,
     replications=10,
     repeat.only=FALSE,
     blocks=1,
     randomize=TRUE,
     seed=72539,
-    nlevels=c(3,3,3),
+    nlevels=c(3,4),
     factor.names=list(
         environment=c("docker","singularity","native"),
-        context=c("mpi"),
         parallelism=c(1,4,8,16)));
 
 export.design(ep_DoE, 
