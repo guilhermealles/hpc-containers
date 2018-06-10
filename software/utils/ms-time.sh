5ed592a10d0544848e7fe0154d16d576ba6be273
Guilherme Alles <guilherme.r.alles@gmail.com>
2018-06-10 13:40:34 -0300

    Changes time measuring script

--- software/utils/ms-time.sh
+++ software/utils/ms-time.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ts=$(date +%s%N) ; $@ > /dev/null 2>&1 ; tt=$((($(date +%s%N) - $ts)/1000000)) ; echo "Time taken: $tt milliseconds"
+ts=$(date +%s%N) ; $@ > /dev/null 2>&1 ; tt=$((($(date +%s%N) - $ts)/1000000)) ; echo "$tt"
