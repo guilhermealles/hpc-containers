d779d46292e521fb6053c7450a7896bfb2b14f81
Guilherme Alles <guilherme.r.alles@gmail.com>
2018-06-10 15:15:06 -0300

    typo

--- wscad/ondes3d/setup/singularity-setup.sh
+++ wscad/ondes3d/setup/singularity-setup.sh
@@ -7,6 +7,6 @@ fi
 SINGULARITY_IMAGES_DIRECTORY=$1
 
 [ -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi ] && rm -rf $SINGULARITY_IMAGES_DIRECTORY/ondes3d
-cp -r ../../../software/ondes3 "$SINGULARITY_IMAGES_DIRECTORY/ondes3d"
+cp -r ../../../software/ondes3d "$SINGULARITY_IMAGES_DIRECTORY/ondes3d"
 sudo singularity build --writable "$SINGULARITY_IMAGES_DIRECTORY/ondes3d.img" ../singularity/ondes3d.bootstrap
 echo "$(cd $SINGULARITY_IMAGES_DIRECTORY && singularity exec ./ondes3d.img ./ondes3d/compile-ondes3d.sh)"
