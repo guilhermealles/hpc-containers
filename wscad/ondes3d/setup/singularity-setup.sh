d409b9e494cf07a4c0820f0e3b6e25f251e95c2b
Guilherme Alles <guilherme.r.alles@gmail.com>
2018-06-17 17:25:06 -0300

    Fixes ondes3d image creation

--- wscad/ondes3d/setup/singularity-setup.sh
+++ wscad/ondes3d/setup/singularity-setup.sh
@@ -6,7 +6,7 @@ if [ -z $1 ]; then
 fi
 SINGULARITY_IMAGES_DIRECTORY=$1
 
-[ -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d-mpi ] && rm -rf $SINGULARITY_IMAGES_DIRECTORY/ondes3d
+[ -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d ] && rm -rf $SINGULARITY_IMAGES_DIRECTORY/ondes3d
 cp -r ../../../software/ondes3d "$SINGULARITY_IMAGES_DIRECTORY/ondes3d"
 sudo singularity build --writable "$SINGULARITY_IMAGES_DIRECTORY/ondes3d.img" ../singularity/ondes3d.bootstrap
 echo "$(cd $SINGULARITY_IMAGES_DIRECTORY && singularity exec ./ondes3d.img ./ondes3d/compile-ondes3d.sh)"
