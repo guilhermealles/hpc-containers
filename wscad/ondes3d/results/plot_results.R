0e2c22e877802f37001ed6fc2008dd1bf7d73adc
Guilherme Alles <guilherme.r.alles@gmail.com>
2018-06-18 10:01:15 -0300

    Adds plot for Ondes3D

---
+++ wscad/ondes3d/results/plot_results.R
@@ -0,0 +1,27 @@
+library(tidyverse);
+
+docker_results <- read_csv('./doe-results.csv');
+sing_native_results <- read_csv('./doe-results-sing-native.csv');
+
+results <- bind_rows(sing_native_results, docker_results)
+results <- results %>%
+  mutate(time = time/1000) %>%
+  group_by(environment, parallelism) %>%
+  summarize(
+    samples = n(),
+    average = mean(time),
+    stdDeviation = sd(time),
+    stdError = 2*stdDeviation/sqrt(samples)
+  );
+
+g <- ggplot(results, aes(x = parallelism, y = average)) + 
+  scale_x_continuous(breaks=c(1, 4, 8, 16), trans='sqrt') + 
+  scale_y_continuous(breaks=seq(0, 32, 4), trans='sqrt') +
+  geom_point(aes(col=environment), size=2) + 
+  geom_line(aes(col=environment), size = 0.5) + 
+  geom_errorbar(aes(ymin=average-stdError, ymax=average+stdError, col=environment), width=0.5) +
+  ggtitle("Ondes3D ESSAI", subtitle = "50 timesteps") +
+  xlab("MPI Processes") +
+  ylab("Execution time(s)");
+
+plot(g);
