e5a827b3a12578170dba3d09f0c572c560c1c437
Guilherme Alles <guilherme.r.alles@gmail.com>
2018-06-29 10:45:02 -0300

    Fix

--- wscad/ondes3d-ligurian/setup/singularity-setup.sh
+++ wscad/ondes3d-ligurian/setup/singularity-setup.sh
@@ -9,7 +9,7 @@ CURRENT_DIRECTORY=$PWD
 
 [ -d $SINGULARITY_IMAGES_DIRECTORY/ondes3d ] && rm -rf $SINGULARITY_IMAGES_DIRECTORY/ondes3d
 cd "$SINGULARITY_IMAGES_DIRECTORY"
-git clone git clone https://guilhermealles@bitbucket.org/schnorr/ondes3d-cases.git
+git clone https://guilhermealles@bitbucket.org/schnorr/ondes3d-cases.git
 mv ondes3d-cases ondes3d
 cp "$CURRENT_DIRECTORY/compile-ondes3d.sh" ondes3d/
 sudo singularity build --writable "$SINGULARITY_IMAGES_DIRECTORY/ondes3d.img" ../singularity/ondes3d.bootstrap
